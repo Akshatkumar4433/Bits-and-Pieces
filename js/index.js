fd55e83bc4ba95fcf784603e1fa4b133d69a88dc
Akshat Kumar <akshatkumar4433@gmail.com>
2022-01-25 22:19:43 -0500

    array of images

--- js/index.js
+++ js/index.js
@@ -1,10 +1,56 @@
+//Fix Vue methods
+
+function ImageManager(splitsX,splitsY) {
+    let imagePieces = [];
+    let key = 0;
+    for(let i =1; i<=splitsX; i++) {
+      for(let j = 1; j<=splitsY; j++ ) {
+          let imagePiece = {}
+          imagePiece['id'] = 'img';
+          imagePiece['src'] = 'images/' +'part' + i + j;
+          imagePiece['key'] = key
+          imagePieces.push(imagePiece);
+          key++;
+      }
+    }
+    return imagePieces;
+}
+
+function shuffleArray(array) {
+     let shuffledArray = [];
+      while(array.length != 0) {
+       let randomNumber = Math.floor(Math.random()*(array.length - 1));
+       shuffledArray.push(array[randomNumber]);
+       array = array.filter(e => e != array[randomNumber]);
+     }
+     return shuffledArray;
+}
+
+
 function byId(id) {
   return document.getElementById(id);
 }
 
+
+
 data = {
     a: 0,
+    imageList: shuffleArray(ImageManager(3,3)),
 }
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+function drag(ev) {
+  ev.dataTransfer.setData('imgId', ev.target.id);
+}
+function drop(ev) {
+  ev.preventDefault();
+  var data = byId(ev.dataTransfer.getData('imgId'))
+  data.style.marginTop = ev.offsetY + 'px';
+  data.style.marginLeft = ev.offsetX + 'px';
+}
+
 
 methods = {
    allowDrop(ev){
@@ -21,4 +67,14 @@ methods = {
    }
 }
 
-var app = new Vue({el:'#app',data, methods})
+
+Vue.component('piece',{
+     methods: methods,
+     props:['pieceInfo'],
+     template: `
+      <img v-bind:src = pieceInfo.src v-bind:id = pieceInfo.id @dragstart="drag($event)">
+     `
+})
+
+//var app = new Vue({el:'#app',data, methods})
+var app = new Vue({el:'#app',data})
